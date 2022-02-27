e8396228149546bdc23e760458cbd9f47458381d
Mouradouchane <mouradouchane@gmail.com>
2022-02-27 10:12:46 +0100

    some improvement in rections side

--- sara_bot.py
+++ sara_bot.py
@@ -1,118 +1,122 @@
-from email import message_from_binary_file
-import json
-import random
+import sys # sys for define external moduels
+import os # for env
+import json # for parse response data 
 import requests 
+
+def define_path(folder_path = ""):
+    sys.path.append(folder_path)
+
+import random
+from email import message_from_binary_file
+from logging import exception
+from jokeapi import Jokes
 import discord # discord moduel important for dealing with discord API's & BOT 
 
-import os
+define_path("./gifs/")
+define_path("./reactions/")
+
+from gif_api import get_gif
+from reaction import get_reaction
+
 # dotenv moduel for loading hidden keys in envirement 
 from dotenv import dotenv_values
 keys = dotenv_values(".env") # load keys from env file
+#import asyncio
 
 # set key's
-discord_key= keys["discord_key"]
-tenor_key=keys["tenor_key"]
-nasa_key=keys["nasa_key"]
-nasa_url=keys["nasa_url"]
+discord_key = keys["discord_key"]
+tenor_key   = keys["tenor_key"]
+nasa_key    = keys["nasa_key"]
+nasa_url    = keys["nasa_url"]
 
-print("=======================")
-print(keys["discord_key"])
-print("=======================")
-
-# joke API for get jokes 
-from jokeapi import Jokes # Import the Jokes class
-import asyncio
-
+# discord controller
 client = discord.Client()
+
+# function who handle sending gif's
+# this function using moduel => "gif/gif_api.py" 
+async def send_gif(message,msg):
+    
+    try: 
+        # function get_gif will try to get gif for you 
+        if len(message) >= 1 :
+
+            gif_path = await get_gif(tenor_key , message[1]) # get gif
+            await msg.channel.send( gif_path ) #send it
+
+        else : 
+            # argument message[1] is missing" 
+            await msg.channel.send( "[!gif] command error 'missing second argument' :sad:" )
+    
+    #  if error happend we will send it as messages
+    except Exception as err:
+        await msg.channel.send("[!gif]  command error missing second argument")
+        await msg.channel.send("```[ERROR] " + str(err)+"```")
+
 
 @client.event
 async def on_ready():
     print("bot {0.user} is online now".format(client))
 
 @client.event
-async def on_message(message):
+async def on_message(msg):
     # get user name only without #....
-    username = str(message.author).split("#")[0]
+    username = str(msg.author).split("#")[0]
     # get user message
-    user_msg = str(message.content)
+    message  = str(msg.content).split(" ")
     # get channel when user send this message
-    channel = str(message.channel.name)
+    channel  = str(msg.channel.name)
 
     # just print message in console as & "log"
-    print(f"[{channel}] {username} : {user_msg}")
+    print(f"[{channel}] {username} : {message}")
 
-    # if this bot it's self don't respont
-    if( message.author == client.user):
+    # if response comming from this bot it's self don't response "just skip"
+    if( msg.author == client.user):
         return
 
+    ### __chat__💬 channel ###
+
     # response to specific channel
-    if message.channel.name == "__chat__💬":
+    if msg.channel.name == "__chat__💬":
         #
         # *** here where answers of messages & commands ***
         #
 
-        # example if user sayed hello
-        if user_msg.lower() == "hello sara": 
-            # sara say hello + his name
-            await message.channel.send(f"hi there , {username}")
-            return
-
-
         # example : give reaction to message
-        if user_msg.lower() == "give me hearth":
+        if message[0].lower() == "give me hearth":
             # send reaction emoji as unicode
-            # UNICODE list : https://unicode.org/emoji/charts/full-emoji-list.html#1f970
-            await message.add_reaction("\U0001F970")
+            # UNICODE list : https://unicode.org/emoji/charts/full-emoji-list.html
+            await msg.add_reaction("\U0001F970")
 
 
         # example : send a joke using jokes API
-        if user_msg.lower() == "give me a joke" or user_msg.lower() == "!joke":
+        if message[0].lower() == "!joke":
             j = await Jokes() # Initialise the class
             joke = await j.get_joke() # Retrieve a random joke
             if joke["type"] == "single": 
                 # send joke
-                await message.channel.send(joke["joke"])
+                await msg.channel.send(joke["joke"])
             else:
-                await message.channel.send(joke["setup"])
-                await message.channel.send(joke["delivery"])
+                await msg.channel.send(joke["setup"])
+                await msg.channel.send(joke["delivery"])
 
 
-        # example : send gif using tenor API
-        if user_msg.lower() == "!gif":
-            # max result comming from gif API
-            max_gifs_request = 8
-            index = random.randint(0,max_gifs_request-1)
-            # send requst for 'smart gift'
-            request = requests.get( "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % ("smart", tenor_key , max_gifs_request) )
-
-            # if respone from server was 200 mean it's GOOD 
-            if request.status_code == 200:
-                # save response 
-                data = json.loads(request.content)["results"]
-                print(data[index]["media"][0]["gif"]["url"])
-                # pick & random gif & send it 
-                await message.channel.send( data[index]["media"][0]["gif"]["url"] )
-            else : 
-                await message.channel.send( "bad response from gif's api :sad:" )
-
-
-        if user_msg.lower() == "!random":
-            await message.channel.send(random.randint(-100,100))
+        if message[0].lower() == "!random":
+            await msg.channel.send(random.randint(-100,100))
 
         # example : using nasa API to get Photo Of the Day
-        if user_msg.lower() == "!nasa":
+        if message[0].lower() == "!nasa":
             req = requests.get(nasa_url+nasa_key)
             if req.status_code == 200:
                 data = json.loads(req.content)
-                await message.channel.send("Nasa Photo of The Day")
-                await message.channel.send(data["url"])
-                await message.channel.send(data["explanation"])
+                await msg.channel.send("Nasa Photo of The Day")
+                await msg.channel.send(data["url"])
+                await msg.channel.send(data["explanation"])
             else:
-                await message.channel.send("Bad Conncetion to Nasa API's")
+                await msg.channel.send("Bad Conncetion to Nasa API's")
 
         # help message 
-        if user_msg.lower() == "!help":
-            await message.channel.send("""
+        if message[0].lower() == "!help":
+            await msg.channel.send("""
 !help           : display commands
 !help command   : display help & examples for specific commands
 !nasa           : display photo of the day from nasa + article
@@ -122,6 +126,33 @@ async def on_message(message):
 !react          : let bot react last message
 hello sara      : let bot say hello for you
             """)
+
+        if message[0].lower() == "!gif":
+            # send_gif function it's the handler for this task
+            await send_gif(message , msg)
+
+    ### general channel ###
+
+    # send gif's using gif api 'tenor'
+    if msg.channel.name == "general":
+
+        if message[0].lower() == "!gif":
+            # send_gif function it's the handler for this task
+            await send_gif(message , msg)
+
+
+    ### for all channels ###
+
+    # example if user sayed hello
+    if msg.content == "hello sara" or msg.content == "hi sara" : 
+        # sara say hello + his name
+        await msg.channel.send(f"hi there , {username}")
+        return
+
+    #send random reaction sometimes
+    if(random.randint(1,100) % 2 == 0): 
+        await msg.add_reaction(get_reaction("empty"))
+
 
 # run bot
 client.run(discord_key)
